2648e6ee4ff507dd299623467140b4d1d3d0afbe
Bozidar Spirovski <spirovski.b@gmail.com>
2022-04-10 14:44:24 +0200

    feat: adding the initial bootstrap 5 templates and home view

--- goya_core/main/views.py
+++ goya_core/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods # To restrict access to views based on the request method 
+
 
 # Create your views here.
+@require_http_methods(["GET"])
+def home_view(request, *args, **kwargs):
+    context = {
+    }
+    return render(request, "home/home.html", context)
---
+++ goya_core/templates/README.md
@@ -0,0 +1,5 @@
+# templates
+This folder contains the html templates that are rendered on the UI when the views send back information.
+The folder structure is identical to the apps folder strucure - each folder contains the templates for the respective app.
+
+All templates are based on the base.html file which is in the root of this folder. 
