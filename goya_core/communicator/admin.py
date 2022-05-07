1483fd176e99c342edd31d7047b24ebf2512f98b
Bozidar Spirovski <spirovski.b@gmail.com>
2022-05-07 20:12:56 +0200

    feat: adding records of sent events for reporting and stats

--- goya_core/communicator/admin.py
+++ goya_core/communicator/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from communicator.models import Latest_Advisory, Latest_Event_Report
+from communicator.models import Latest_Advisory, Latest_Event_Report, Advisories_Sent, Events_Sent
 
 # Register your models here.
 
@@ -15,3 +15,17 @@ class Latest_Event_ReportAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Latest_Event_Report, Latest_Event_ReportAdmin)
+
+
+class Advisories_SentAdmin(admin.ModelAdmin):
+    list_display = ('advised_workspace', 'advisory_sent', 'advisory_sent_time')
+
+
+admin.site.register(Advisories_Sent, Advisories_SentAdmin)
+
+
+class Events_SentAdmin(admin.ModelAdmin):
+    list_display = ('advised_workspace', 'event_sent', 'event_sent_time')
+
+
+admin.site.register(Events_Sent, Events_SentAdmin)
